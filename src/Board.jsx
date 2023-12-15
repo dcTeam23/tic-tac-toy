f135c5d4b653c423fb82b6b34419305041ddebf0
AbraaoAlves <abraao.teodosio@gmail.com>
2023-12-15 11:52:32 -0300

    chore(Board): add  board  state

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,23 +1,35 @@
+import {useState} from 'react'
 import {Square} from './Square'
 
+
 export function Board() {
+  const SQUARES = [
+  
+    '',  'O', 'X', 
+    '',  'O', 'X', 
+    'X', 'O', 'Z'
+
+  ];
+  
+  const [squares1, setSquare] = useState(SQUARES);
+  
   return (
     <>
       <div>
-        <Square value={1}/>
-        <Square value={2}/>
-        <Square value={3}/>
+        <Square value={squares1[1-1]}/>
+        <Square value={squares1[2-1]}/>
+        <Square value={squares1[3-1]}/>
       </div>
       <div>
-        <Square value={4}/>
-        <Square value={5}/>
-        <Square value={6}/>
+        <Square value={squares1[4-1]}/>
+        <Square value={squares1[5-1]}/>
+        <Square value={squares1[6-1]}/>
       </div>
 
       <div>
-        <Square value={7}/>
-        <Square value={8}/>
-        <Square value={9}/>
+        <Square value={squares1[7-1]}/>
+        <Square value={squares1[8-1]}/>
+        <Square value={squares1[9-1]}/>
       </div>
     </>
   );
