9f8329e04d1e57ba86cb9c525e6abd4699ecad0d
AbraaoAlves <abraao.teodosio@gmail.com>
2023-12-15 11:09:53 -0300

    feat(Square): add value prop

--- src/Square.jsx
+++ src/Square.jsx
@@ -1,4 +1,4 @@
 
-export function Square() {
-  return <button className="square">1</button>        
+export function Square({ value }) {
+  return <button className="square">{value}</button>        
 }
