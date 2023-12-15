fae05f62b23278578dfe762c525e700b21e4bf17
AbraaoAlves <abraao.teodosio@gmail.com>
2023-12-15 11:02:43 -0300

    chore: replace button by square

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,21 +1,23 @@
+import {Square} from './Square'
+
 export function Board() {
   return (
     <>
       <div>
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div>
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
 
       <div>
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
