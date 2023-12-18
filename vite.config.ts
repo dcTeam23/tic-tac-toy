1b7207436c85b13d5e119e2d0b08646b8f80e7d9
AbraaoAlves <abraao.teodosio@gmail.com>
2023-12-17 23:31:05 -0300

    chore: set base and app name

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/tic-tac-toy/',
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/test/setup.js'
+    setupFiles: './src/test/setup.js',
   },
-})
+});
