3e408d22024a0ee35e0ef6f21756c450266f55e7
AbraaoAlves <abraao.teodosio@gmail.com>
2023-12-17 22:39:43 -0300

    fix(Board): handle click by index and value

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -7,27 +7,37 @@ export function Board() {
   const [squares, setSquare] = useState(SQUARES);
   const [nextIsX, setNextIsX] = useState(true);
 
-  const handleClick = () => {
+  /**
+   * @param {number} i
+   */
+  const handleClick = (i) => () => {
+    if (nextIsX) {
+      squares[i] = 'X';
+    } else {
+      squares[i] = 'O';
+    }
+
+    setSquare(squares);
     setNextIsX(!nextIsX);
   };
 
   return (
     <>
       <div>
-        <Square onClick={handleClick} value={squares[0]} />
-        <Square onClick={handleClick} value={squares[1]} />
-        <Square onClick={handleClick} value={squares[2]} />
+        <Square onClick={handleClick(0)} value={squares[0]} />
+        <Square onClick={handleClick(1)} value={squares[1]} />
+        <Square onClick={handleClick(2)} value={squares[2]} />
       </div>
       <div>
-        <Square onClick={handleClick} value={squares[3]} />
-        <Square onClick={handleClick} value={squares[4]} />
-        <Square onClick={handleClick} value={squares[5]} />
+        <Square onClick={handleClick(3)} value={squares[3]} />
+        <Square onClick={handleClick(4)} value={squares[4]} />
+        <Square onClick={handleClick(5)} value={squares[5]} />
       </div>
 
       <div>
-        <Square onClick={handleClick} value={squares[6]} />
-        <Square onClick={handleClick} value={squares[7]} />
-        <Square onClick={handleClick} value={squares[8]} />
+        <Square onClick={handleClick(6)} value={squares[6]} />
+        <Square onClick={handleClick(7)} value={squares[7]} />
+        <Square onClick={handleClick(8)} value={squares[8]} />
       </div>
     </>
   );
