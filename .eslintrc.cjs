d1cbe0bd40e508da5bd49820ecc543a6e05c66cc
AbraaoAlves <abraao.teodosio@gmail.com>
2023-12-17 21:59:02 -0300

    feat(lint): config prettier + lint
    - add and config prettier
    - config lint with vitest
    - add settings.json to vscode config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,12 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2021: true, "vitest-globals/env": true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:vitest-globals/recommended',
+    'eslint-config-prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -14,5 +16,15 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'no-unused-vars': [
+      'error',
+      {
+        'vars': 'all',
+        'args': 'after-used',
+        'ignoreRestSiblings': true,
+        'argsIgnorePattern': '^_'
+      }
+    ],
+    'react/react-in-jsx-scope': 'off'
   },
 }
