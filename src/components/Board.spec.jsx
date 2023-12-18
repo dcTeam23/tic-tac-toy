891b02c27482d57c8878b40138d8ec37e1951b35
AbraaoAlves <abraao.teodosio@gmail.com>
2023-12-17 22:05:27 -0300

    chore: format code

--- src/components/Board.spec.jsx
+++ src/components/Board.spec.jsx
@@ -1,28 +1,26 @@
-import { render } from "@testing-library/react";
-import { Board } from "./Board";
+import { render } from '@testing-library/react';
+import { Board } from './Board';
 
-describe("Board", () => {
+describe('Board', () => {
   /** @type {import("@testing-library/react").RenderResult} */
   let utils;
   beforeEach(() => (utils = render(<Board />)));
 
-  describe("render state with buttons", () => {
+  describe('render state with buttons', () => {
     /** @type {HTMLButtonElement[]} */
     let buttons;
 
-    beforeEach(() => (buttons = utils.getAllByRole("button")));
+    beforeEach(() => (buttons = utils.getAllByRole('button')));
 
-    it("with 9 buttons", () => {
+    it('with 9 buttons', () => {
       expect(buttons).toHaveLength(9);
     });
 
-    it("with empty state in each button", () => {
+    it('with empty state in each button', () => {
       buttons.forEach((btn) => expect(btn).toBeEmptyDOMElement());
     });
   });
 
-  it.todo("render state with 9 buttons");
-  it.todo("render state with empty buttons");
-  it.todo("handle taking turn");
-  it.todo("declare winner");
+  it.todo('handle taking turn');
+  it.todo('declare winner');
 });
